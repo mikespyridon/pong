6e6353304c297b748d69c3fac9dd72c2223c7995
mikespyridon <120049095+mikespyridon@users.noreply.github.com>
2023-12-14 15:44:40 -0500

    Add score text in game window

--- game.py
+++ game.py
@@ -15,11 +15,19 @@ clock = pygame.time.Clock()
 paddle_left = Paddle('computer', (20, (HEIGHT / 2)), 'green')
 paddle_right = Paddle('player', ((WIDTH - 20), (HEIGHT / 2)), 'white')
 
+#groups
 paddle_group = pygame.sprite.Group(paddle_left, paddle_right)
+
+#score text
+score = 0
+text_font = pygame.font.SysFont('Arial', 30, True)
+score_text = text_font.render(f'Score: {score}', True, 'white')
+text_width = score_text.get_width()
 
 while True:
   #update screen
   screen.fill((0,0,0))
+  screen.blit(score_text, (((WIDTH / 2) - (text_width / 2)), 25))
   
   paddle_group.update()
   
